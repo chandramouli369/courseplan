30be9a01c59bffdba20c966a58e93e7def2f3195
mouli thammineni <58020994+mouli-coder@users.noreply.github.com>
2021-03-05 19:14:50 +0530

    Add files via upload

---
+++ paper/test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <script type="text/javascript" src="../js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+</head>
+<body>
+	<script>
+		$(document).trigger($.Event("keypress", { keyCode: 97 }));
+	</script>
+</body>
+</html>
